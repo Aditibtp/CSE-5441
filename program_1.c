e0d89da34135ceb94c218eec46052e65f6ba8bea
Aditi <aditisharmabtp@gmail.com>
2019-10-17 16:34:50 -0400

    minor updates

--- program_1.c
+++ program_1.c
@@ -21,10 +21,6 @@ typedef struct Grid_boxes{
   int *right_list;
 } Grid_box;
 
-char* strsinline(char *){
-    
-}
-
 int main(int argc, char *argv[])
 {
   char *newline;
@@ -34,14 +30,15 @@ int main(int argc, char *argv[])
   int row = 0;
   int col = 0;
   char line[256];
-  int i=0;
+  int linecounter = 0;
   char delim[] = " ";
-  
+  int i=0;
+  int j=0;
   //reading first char
   if(fgets(line, sizeof(line), stdin)){
     
     int n = strlen(line);
-
+    i=0;
 	char *ptr = strtok(line, delim);
 
 	while(ptr != NULL)
@@ -60,7 +57,7 @@ int main(int argc, char *argv[])
 	}
 
   }
-  i=0;
+  
   printf("total boxes are  %d\n", total_boxes);
   Grid_box *grid_boxes = malloc(sizeof(Grid_box) * total_boxes);
 
@@ -68,28 +65,105 @@ int main(int argc, char *argv[])
   while (fgets(line, sizeof(line), stdin)) {
       printf("%s\n", line);
       int n = strlen(line);
-
+      Grid_box gb;
+      i=0;
       char *ptr = strtok(line, delim);
-    
-      while(ptr != NULL)
-      {
-    	//printf("%s\n", ptr);
-    	if(ptr && i==0){
-    		   total_boxes = (int) strtol(ptr, (char **)NULL, 10);
-    	}else if(ptr && i==1){
-    	    row = (int) strtol(ptr, (char **)NULL, 10);
-    	}else if(ptr){
-    	    col = (int) strtol(ptr, (char **)NULL, 10);
-    	}
-    	i++;
-    	ptr = strtok(NULL, delim);
-    		
-     }
+      if(linecounter == 0){
+          printf("%s\n", ptr);
+          gb.box_id = (int) strtol(ptr, (char **)NULL, 10);
+          
+      }else if(linecounter == 1){
+        printf("%s\n", ptr);
+        while(ptr != NULL)
+          {
+        	//printf("%s\n", ptr);
+        	if(ptr && i==0){
+        		gb.xc = (int) strtol(ptr, (char **)NULL, 10);
+        	}else if(ptr && i==1){
+        	    gb.yc = (int) strtol(ptr, (char **)NULL, 10);
+        	}else if(i==2 && ptr){
+        	    gb.height = (int) strtol(ptr, (char **)NULL, 10);
+        	}else if(ptr){
+        	    gb.width = (int) strtol(ptr, (char **)NULL, 10);
+        	}
+        	i++;
+        	ptr = strtok(NULL, delim);
+        		
+         }
+      }else if(linecounter == 2){
+          i=0;
+          j=0;
+          while(ptr != NULL)
+          {
+        	//printf("%s\n", ptr);
+        	if(ptr && i==0){
+        		gb.top_n = (int) strtol(ptr, (char **)NULL, 10);
+        		gb.top_list = malloc(sizeof(int) * gb.top_n);
+        	}else if(ptr){
+        	    gb.top_list[j++] = (int) strtol(ptr, (char **)NULL, 10);
+        	    j++;
+        	}
+        	i++;
+        	ptr = strtok(NULL, delim);
+        		
+         }
+      }else if(linecounter == 3){
+          i=0;
+          j=0;
+          while(ptr != NULL)
+          {
+        	//printf("%s\n", ptr);
+        	if(ptr && i==0){
+        		gb.bot_n = (int) strtol(ptr, (char **)NULL, 10);
+        		gb.bot_list = malloc(sizeof(int) * gb.bot_n);
+        	}else if(ptr){
+        	    gb.bot_list[j++] = (int) strtol(ptr, (char **)NULL, 10);
+        	    j++;
+        	}
+        	i++;
+        	ptr = strtok(NULL, delim);
+        		
+         }
+      }else if(linecounter == 4){
+          i=0;
+          j=0;
+          while(ptr != NULL)
+          {
+        	//printf("%s\n", ptr);
+        	if(ptr && i==0){
+        		gb.left_n = (int) strtol(ptr, (char **)NULL, 10);
+        		gb.left_list = malloc(sizeof(int) * gb.left_n);
+        	}else if(ptr){
+        	    gb.left_list[j++] = (int) strtol(ptr, (char **)NULL, 10);
+        	    j++;
+        	}
+        	i++;
+        	ptr = strtok(NULL, delim);
+        		
+         }
+      }else if(linecounter == 5){
+          i=0;
+          j=0;
+          while(ptr != NULL)
+          {
+        	//printf("%s\n", ptr);
+        	if(ptr && i==0){
+        		gb.right_n = (int) strtol(ptr, (char **)NULL, 10);
+        		gb.left_list = malloc(sizeof(int) * gb.right_n);
+        	}else if(ptr){
+        	    gb.right_list[j++] = (int) strtol(ptr, (char **)NULL, 10);
+        	    j++;
+        	}
+        	i++;
+        	ptr = strtok(NULL, delim);
+        		
+         }
+      }else if(linecounter == 6){
+          gb.temp = (int) strtol(ptr, (char **)NULL, 10);
+      }
+      linecounter++;
   }
   
-
-  
-
   printf("\n");
 
   return 0;
