7d9c523bbe7d50a0a50740f363990ead8248407d
Aditi <aditisharmabtp@gmail.com>
2019-10-17 17:00:33 -0400

    parsed all the information

--- program_1.c
+++ program_1.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
   int k=0;
   int row = 0;
   int col = 0;
-  char line[256];
+  char line[500];
   int linecounter = 0;
   char delim[] = " ";
   int i=0;
@@ -63,20 +63,22 @@ int main(int argc, char *argv[])
 
   
   while (fgets(line, sizeof(line), stdin)) {
-      printf("%s\n", line);
+      //printf("%s\n", line);
       int n = strlen(line);
       Grid_box gb;
       i=0;
       char *ptr = strtok(line, delim);
+      if(linecounter == 7) linecounter = 0;
       if(linecounter == 0){
-          printf("%s\n", ptr);
+          //printf("%s\n", ptr);
           gb.box_id = (int) strtol(ptr, (char **)NULL, 10);
           
       }else if(linecounter == 1){
         printf("%s\n", ptr);
+        i=0;
         while(ptr != NULL)
           {
-        	//printf("%s\n", ptr);
+        //	printf("linecounter 1 %s\n", ptr);
         	if(ptr && i==0){
         		gb.xc = (int) strtol(ptr, (char **)NULL, 10);
         	}else if(ptr && i==1){
@@ -91,74 +93,82 @@ int main(int argc, char *argv[])
         		
          }
       }else if(linecounter == 2){
-          i=0;
-          j=0;
-          while(ptr != NULL)
+        printf("%s\n", ptr);
+        j=0;
+        i=0;
+        while(ptr != NULL)
           {
-        	//printf("%s\n", ptr);
+        //	printf("linecounter 2 %s\n", ptr);
         	if(ptr && i==0){
         		gb.top_n = (int) strtol(ptr, (char **)NULL, 10);
-        		gb.top_list = malloc(sizeof(int) * gb.top_n);
-        	}else if(ptr){
-        	    gb.top_list[j++] = (int) strtol(ptr, (char **)NULL, 10);
-        	    j++;
+        		gb.top_list = malloc(sizeof(int)*(gb.top_n));
+        	}else if(ptr && i==1){
+        	   gb.top_list[j] = (int) strtol(ptr, (char **)NULL, 10);
+        	   j++;
         	}
         	i++;
         	ptr = strtok(NULL, delim);
         		
          }
       }else if(linecounter == 3){
-          i=0;
-          j=0;
-          while(ptr != NULL)
+        printf("%s\n", ptr);
+        j=0;
+        i=0;
+        while(ptr != NULL)
           {
-        	//printf("%s\n", ptr);
+        //	printf("linecounter 3 %s\n", ptr);
         	if(ptr && i==0){
         		gb.bot_n = (int) strtol(ptr, (char **)NULL, 10);
-        		gb.bot_list = malloc(sizeof(int) * gb.bot_n);
-        	}else if(ptr){
-        	    gb.bot_list[j++] = (int) strtol(ptr, (char **)NULL, 10);
-        	    j++;
+        		gb.bot_list = malloc(sizeof(int)*(gb.bot_n));
+        	}else if(ptr && i==1){
+        	   gb.bot_list[j] = (int) strtol(ptr, (char **)NULL, 10);
+        	   //printf("bottom neighbor of %d is %d \n", gb.box_id, gb.bot_list[j]);
+        	   j++;
         	}
         	i++;
         	ptr = strtok(NULL, delim);
         		
          }
       }else if(linecounter == 4){
-          i=0;
-          j=0;
-          while(ptr != NULL)
+        printf("%s\n", ptr);
+        j=0;
+        i=0;
+        while(ptr != NULL)
           {
-        	//printf("%s\n", ptr);
+        //	printf("linecounter 4 %s\n", ptr);
         	if(ptr && i==0){
         		gb.left_n = (int) strtol(ptr, (char **)NULL, 10);
-        		gb.left_list = malloc(sizeof(int) * gb.left_n);
-        	}else if(ptr){
-        	    gb.left_list[j++] = (int) strtol(ptr, (char **)NULL, 10);
-        	    j++;
+        		gb.left_list = malloc(sizeof(int)*(gb.left_n));
+        	}else if(ptr && i==1){
+        	   gb.left_list[j] = (int) strtol(ptr, (char **)NULL, 10);
+        	   //printf("bottom neighbor of %d is %d \n", gb.box_id, gb.bot_list[j]);
+        	   j++;
         	}
         	i++;
         	ptr = strtok(NULL, delim);
         		
          }
       }else if(linecounter == 5){
-          i=0;
-          j=0;
-          while(ptr != NULL)
+        printf("%s\n", ptr);
+        j=0;
+        i=0;
+        while(ptr != NULL)
           {
-        	//printf("%s\n", ptr);
+        	//printf("linecounter 5 %s\n", ptr);
         	if(ptr && i==0){
         		gb.right_n = (int) strtol(ptr, (char **)NULL, 10);
-        		gb.left_list = malloc(sizeof(int) * gb.right_n);
-        	}else if(ptr){
-        	    gb.right_list[j++] = (int) strtol(ptr, (char **)NULL, 10);
-        	    j++;
+        		gb.right_list = malloc(sizeof(int)*(gb.right_n));
+        	}else if(ptr && i==1){
+        	   gb.right_list[j] = (int) strtol(ptr, (char **)NULL, 10);
+        	   printf("bottom neighbor of %d is %d \n", gb.box_id, gb.right_list[j]);
+        	   j++;
         	}
         	i++;
         	ptr = strtok(NULL, delim);
         		
          }
       }else if(linecounter == 6){
+          printf("temprature %s\n", ptr);
           gb.temp = (int) strtol(ptr, (char **)NULL, 10);
       }
       linecounter++;
