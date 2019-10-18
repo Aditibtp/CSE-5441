9f5b95328e54b08cc381c320c424a09f6292eaf6
Aditi <aditisharmabtp@gmail.com>
2019-10-18 10:56:20 -0400

    fixed printing

--- program_1.c
+++ program_1.c
@@ -1,25 +1,8 @@
-/******************************************************************************
-* FILE: hello.c
-* DESCRIPTION:
-*   A "hello world" Pthreads program.  Demonstrates thread creation and
-*   termination.
-* AUTHOR: Blaise Barney
-* LAST REVISED: 08/09/11
-******************************************************************************/
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#define NUM_THREADS	5
-
-void *PrintHello(void *threadid)
-{
-   long tid;
-   tid = (long)threadid;
-   printf("Hello World! It's me, thread #%ld!\n", tid);
-   pthread_exit(NULL);
-}
 
 typedef struct Grid_boxes{
   int box_id;
@@ -28,63 +11,205 @@ typedef struct Grid_boxes{
   int bot_n;
   int left_n;
   int right_n;
+  int xc;
+  int yc;
+  int height;
+  int width;
   int *top_list;
   int *bot_list;
   int *left_list;
   int *right_list;
 } Grid_box;
 
-
-
 int main(int argc, char *argv[])
 {
-   /*pthread_t threads[NUM_THREADS];
-   int rc;
-   long t;
-   for(t=0;t<NUM_THREADS;t++){
-     printf("In main: creating thread %ld\n", t);
-     rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
-     if (rc){
-       printf("ERROR; return code from pthread_create() is %d\n", rc);
-       exit(-1);
-       }
-     }
-
-   // Last thing that main() should do 
-   pthread_exit(NULL);*/
-
-  FILE *file;
-  char filename[200];
   char *newline;
   int total_boxes = 0;
   int index = 0;
   int k=0;
+  int row = 0;
+  int col = 0;
+  char line[500];
+  int linecounter = 0;
+  char delim[] = " ";
+  int i=0;
+  int j=0;
+  //reading first char
+  if(fgets(line, sizeof(line), stdin)){
+    
+    int n = strlen(line);
+    i=0;
+  char *ptr = strtok(line, delim);
 
-  while(!feof(stdin) && k != -1){
-    scanf("%d", &k);
-    printf("some val %d\n", k);
-    //arr[index] = k;
-    index++;
+  while(ptr != NULL)
+  {
+    //printf("%s\n", ptr);
+    if(ptr && i==0){
+        total_boxes = (int) strtol(ptr, (char **)NULL, 10);
+    }else if(ptr && i==1){
+        row = (int) strtol(ptr, (char **)NULL, 10);
+    }else if(ptr){
+        col = (int) strtol(ptr, (char **)NULL, 10);
+    }
+    i++;
+    ptr = strtok(NULL, delim);
+    
   }
 
-
-
-  //Grid_box *gbs = malloc(total_boxes * sizeof(Grid_box));
-
-  //if(fgets(filename, sizeof(filename), stdin) == NULL)
-    //return -1;
-  //newline = strchr(filename, '\n');
-
-  //if(newline)
-    //*newline = '\0';
-  //printf("****%s*****\n", filename);
-
-  //file = fopen(filename, "r");
-
+  }
   
+  printf("total boxes are  %d\n", total_boxes);
+  Grid_box *grid_boxes = malloc(sizeof(Grid_box) * total_boxes);
+  int t=0;
+  
+  while (fgets(line, sizeof(line), stdin)) {
+      //printf("%s\n", line);
+      int n = strlen(line);
+      Grid_box gb;
+      i=0;
+      char *ptr = strtok(line, delim);
+      if(linecounter == 7) linecounter = 0;
+      if(linecounter == 0){
+          //printf("%s\n", ptr);
+          gb.box_id = (int) strtol(ptr, (char **)NULL, 10);
+          
+      }else if(linecounter == 1){
+        //printf("%s\n", ptr);
+        i=0;
+        while(ptr != NULL)
+          {
+        //  printf("linecounter 1 %s\n", ptr);
+          if(ptr && i==0){
+            gb.xc = (int) strtol(ptr, (char **)NULL, 10);
+          }else if(ptr && i==1){
+              gb.yc = (int) strtol(ptr, (char **)NULL, 10);
+          }else if(i==2 && ptr){
+              gb.height = (int) strtol(ptr, (char **)NULL, 10);
+          }else if(ptr){
+              gb.width = (int) strtol(ptr, (char **)NULL, 10);
+          }
+          i++;
+          ptr = strtok(NULL, delim);
+            
+         }
+      }else if(linecounter == 2){
+       // printf("%s\n", ptr);
+        j=0;
+        i=0;
+        while(ptr != NULL)
+          {
+        //  printf("linecounter 2 %s\n", ptr);
+          if(ptr && i==0){
+            gb.top_n = (int) strtol(ptr, (char **)NULL, 10);
+            gb.top_list = malloc(sizeof(int)*(gb.top_n));
+          }else if(ptr && i==1){
+             gb.top_list[j] = (int) strtol(ptr, (char **)NULL, 10);
+             j++;
+          }
+          i++;
+          ptr = strtok(NULL, delim);
+            
+         }
+      }else if(linecounter == 3){
+        printf("%s\n", ptr);
+        j=0;
+        i=0;
+        while(ptr != NULL)
+          {
+        //  printf("linecounter 3 %s\n", ptr);
+          if(ptr && i==0){
+            gb.bot_n = (int) strtol(ptr, (char **)NULL, 10);
+            gb.bot_list = malloc(sizeof(int)*(gb.bot_n));
+          }else if(ptr && i==1){
+             gb.bot_list[j] = (int) strtol(ptr, (char **)NULL, 10);
+             //printf("bottom neighbor of %d is %d \n", gb.box_id, gb.bot_list[j]);
+             j++;
+          }
+          i++;
+          ptr = strtok(NULL, delim);
+            
+         }
+      }else if(linecounter == 4){
+        //printf("%s\n", ptr);
+        j=0;
+        i=0;
+        while(ptr != NULL)
+          {
+        //  printf("linecounter 4 %s\n", ptr);
+          if(ptr && i==0){
+            gb.left_n = (int) strtol(ptr, (char **)NULL, 10);
+            gb.left_list = malloc(sizeof(int)*(gb.left_n));
+          }else if(ptr && i==1){
+             gb.left_list[j] = (int) strtol(ptr, (char **)NULL, 10);
+             //printf("bottom neighbor of %d is %d \n", gb.box_id, gb.bot_list[j]);
+             j++;
+          }
+          i++;
+          ptr = strtok(NULL, delim);
+            
+         }
+      }else if(linecounter == 5){
+       // printf("%s\n", ptr);
+        j=0;
+        i=0;
+        while(ptr != NULL)
+          {
+          //printf("linecounter 5 %s\n", ptr);
+          if(ptr && i==0){
+            gb.right_n = (int) strtol(ptr, (char **)NULL, 10);
+            gb.right_list = malloc(sizeof(int)*(gb.right_n));
+          }else if(ptr && i==1){
+             gb.right_list[j] = (int) strtol(ptr, (char **)NULL, 10);
+             //printf("bottom neighbor of %d is %d \n", gb.box_id, gb.right_list[j]);
+             j++;
+          }
+          i++;
+          ptr = strtok(NULL, delim);
+            
+         }
+      }else if(linecounter == 6){
+          //printf("temprature %s\n", ptr);
+          gb.temp = (int) strtol(ptr, (char **)NULL, 10);
+      }
+      linecounter++;
+      if(linecounter == 7){
+          grid_boxes[t] = gb;
+          t++;
+      }
+  }
+  
+  //printing all the grid boxes
+  
+  for(int i=0; i<total_boxes; i++){
+      printf("box id %d\n", grid_boxes[i].box_id);
+      printf("box temprature %d\n", grid_boxes[i].temp);
+      printf("box x %d\n", grid_boxes[i].xc);
+      printf("box y %d\n", grid_boxes[i].yc);
+      printf("box height and width %d    %d\n", grid_boxes[i].height, grid_boxes[i].width);
+      
+      printf("Left neighbours: ");
+      for(int j=0; j<grid_boxes[i].left_n; j++){
+        printf("%d ", grid_boxes[i].left_list[j]);
+      }
+      printf("\nRight neighbours: ");
 
+      for(int j=0; j<grid_boxes[i].right_n; j++){
+        printf("%d ", grid_boxes[i].right_list[j]);
+      }
+      printf("\ntop neighbours: ");
+
+      for(int j=0; j<grid_boxes[i].top_n; j++){
+        printf("%d ", grid_boxes[i].top_list[j]);
+      }
+      printf("\nbottom neighbours: ");
+
+      for(int j=0; j<grid_boxes[i].bot_n; j++){
+        printf("%d ", grid_boxes[i].bot_list[j]);
+      }
+
+      printf("\n*******\n");
+      
+  }
   printf("\n");
-
   return 0;
-
 }
