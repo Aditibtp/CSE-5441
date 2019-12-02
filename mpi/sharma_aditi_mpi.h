f65942130a7119237e62bb9b73add9abc9545b0e
Aditi <aditisharmabtp@gmail.com>
2019-12-01 22:10:03 -0500

    Update sharma_aditi_mpi.h

--- mpi/sharma_aditi_mpi.h
+++ mpi/sharma_aditi_mpi.h
@@ -24,22 +24,25 @@ int **left_list;
 int **right_list;
 
 /* Structure sent as param to thread safe func */
-typedef struct Box_range{
-  int p_rank; //thread ID.
+typedef struct Thread_range{
+  int thread_id; //thread ID.
   int start; //start grid index
   int end; // end grid index
-} box_range;
+} thread_range;
 
 #define DBL_MAX 1.7976931348623158e+308 /* max value */
 #define DBL_MIN 2.2250738585072014e-308 /* min positive value */
 
+#define NUM_WORKERS 4
+
 int total_boxes = 0;
 Grid_box *grid_boxes;
-double *dsv_c = NULL;
+
 double epsilon = 0.1;
 double affect_rate = 0.1;
-int num_threads_req = 4;
-int num_process = 4;
+int world_size = 5;
+int num_threads_req = 1;
+int num_process = 5;
 double cur_min_dsv = DBL_MAX;
 double cur_max_dsv = DBL_MIN;
 
@@ -51,6 +54,7 @@ int imin(int a, int b);
 
 void printBoxes();
 
-void calculateDsvForBox(int box_index);
+void calculateDsvForBox(Grid_box *grid_boxes, int **top_list, int **bot_list, int **left_list, int **right_list, int box_index);
 
-void compute_dsv(thread_range *mydata);
+
+
