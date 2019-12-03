0469d706367738c00d47cd42d8ca7cebc5f109fb
Aditi <aditisharmabtp@gmail.com>
2019-12-02 19:28:20 -0500

    Update sharma_aditi_mpi.h

--- mpi/sharma_aditi_mpi.h
+++ mpi/sharma_aditi_mpi.h
@@ -38,8 +38,8 @@ typedef struct Thread_range{
 int total_boxes = 0;
 Grid_box *grid_boxes;
 
-double epsilon = 0.1;
-double affect_rate = 0.1;
+double epsilon = 0.002;
+double affect_rate = 0.08;
 int world_size = 5;
 int num_threads_req = 1;
 int num_process = 5;
@@ -54,7 +54,7 @@ int imin(int a, int b);
 
 void printBoxes();
 
-void calculateDsvForBox(Grid_box *grid_boxes, int **top_list, int **bot_list, int **left_list, int **right_list, int box_index);
+void calculateDsvForBox(Grid_box *grid_boxes, int **top_list, int **bot_list, int **left_list, int **right_list, int box_index, double *dsv_c);
 
 
 
